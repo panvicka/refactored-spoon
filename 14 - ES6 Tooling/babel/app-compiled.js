062b7aa730dae28dff75bd9ad32dbe67f757cbd2
panvicka <panvicka@gmail.com>
2020-09-14 19:51:43 +0200

    babel etc

---
+++ 14 - ES6 Tooling/babel/app-compiled.js
@@ -0,0 +1,6 @@
+//const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+var age = 100;
+var people = ['Wes', 'Kait'];
+var fullNames = people.map(function (name) {
+  return "".concat(name, " Bos");
+});
