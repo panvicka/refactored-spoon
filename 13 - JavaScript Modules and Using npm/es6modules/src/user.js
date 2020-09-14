c1a2056e279a8b355bff929fdddb8a6b3636d35a
panvicka <panvicka@gmail.com>
2020-09-14 18:47:16 +0200

    webpack exercise

---
+++ 13 - JavaScript Modules and Using npm/es6modules/src/user.js
@@ -0,0 +1,21 @@
+import slug from "slug";
+import base64 from "base-64";
+import { url } from "./config";
+
+export default function User(name, email, website) {
+  return {
+    name,
+    email,
+    website,
+  };
+}
+
+export function createUrl(name) {
+  return `${url}/users/${slug(name)}`;
+}
+
+export function gravatar(email) {
+  const hash = base64.encode(email);
+  const photoUrl = `https://www.gravatar.com/avatar/${hash}`;
+  return photoUrl;
+}
