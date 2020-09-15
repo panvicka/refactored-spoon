da078841a2c18517a7e266d71c053e86fa931d0e
panvicka <panvicka@gmail.com>
2020-09-15 18:34:54 +0200

    classes, base info

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -3,9 +3,17 @@ import { uniq } from "lodash";
 import {
   apiKey as myApiKey, url, sayHi, oldAge, dog,
 } from "./src/config";
+import User, { createUrl, gravatar } from "./src/user";
 
 console.log(myApiKey, url, oldAge, dog);
 sayHi("Ren");
 
 const ages = [1, 1, 4, 52, 12, 4];
 console.log(uniq(ages));
+
+const rendlik = new User("Ren Dlik", "rendlik@gmail.com", "wesbos.com");
+console.log(rendlik);
+const profile = createUrl(rendlik.name);
+console.log(profile);
+const profilePic = gravatar(rendlik.email);
+console.log(profilePic);
---
+++ 14 - ES6 Tooling/systemjs/checkout.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export function addTax(amount, taxRate) {
+  return amount + (amount + taxRate);
+}
